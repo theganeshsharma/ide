1479d6896010c17e73189b4630aadc665ca5ddd6
Umair Khan <omerjerk@gmail.com>
2016-12-26 12:25:02 +0530

    convert to c
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by umair on 26/12/16.
+ */
+
+$(document).ready(function () {
+    var URL = "http://judge.cb.lk/api/";
+    $('#submit').click(function () {
+        var lang = "c";
+        var editor = ace.edit("editor");
+        var source = editor.getValue();
+        console.log('source - ' + source);
+        // axios.post('/');
+    });
+});
