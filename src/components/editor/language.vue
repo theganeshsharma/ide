31a1c95c38737e51cf717be2b10284730502dfc7
sahilharjai <sahil.harjai99@gmail.com>
2017-10-01 21:45:22 +0530

    Dropdown Bug Fixed (#127)
    * Dropdown Bug Fixed
    
    https://github.com/coding-blocks/ide/issues/124

--- src/components/editor/language.vue
+++ src/components/editor/language.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="btn-group" :class="{ open : isOpen}" @click="open">
+  <div class="btn-group" :class="{ open : isOpen}"  @click="open">
     <button id="panelLang" type="button" class="btn btn-sm btn-menu"
-            data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
+            aria-haspopup="true" aria-expanded="false" @blur="close" >
       <span v-if="selected"> {{selected}} </span> <span class="caret"></span>
        <i class="fa fa-code" aria-hidden="true"></i>
     </button>
     <ul class="dropdown-menu">
-      <li v-for="option in options" @click="select(option)"><a href="#">{{option}}</a></li>
-    </ul>
+        <li v-for="option in options" @click="select(option)"><a href="#">{{option}}</a></li>
+    </ul>  
   </div>
 </template>
 
@@ -33,11 +32,15 @@
     },
     methods: {
       select(selected) {
-        this.$store.commit('changeLanguage', selected)
+          this.$store.commit('changeLanguage', selected) 
+
       },
       open() {
-        this.isOpen = !this.isOpen
+        this.isOpen = !this.isOpen 
+      },
+      close () {
+        setTimeout(() => { this.isOpen=false },250 )    
       }
     }
-  }
+  } 
 </script>
