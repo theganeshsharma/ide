3e7dcf06b98b1f367fa0666d7e3ed592f0cfc050
Arnav Gupta <dev@championswimmer.in>
2018-11-16 15:44:52 +0530

    login button highlight
    @prateek27 bhaiya ko khush karo! 😉

--- src/components/auth/LoginButton.vue
+++ src/components/auth/LoginButton.vue
@@ -16,7 +16,7 @@
        Logout <span class="fas fa-sign-in-alt"></span>
     </button>
   </span>
-  <button id="panelLang" type="button" class="btn btn-sm btn-menu"
+  <button id="panelLang" type="button" class="btn btn-sm btn-outline-danger"
     @click="login"
     v-else >
     Login <i class="fas fa-sign-in-alt"></i>
